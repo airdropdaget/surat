9dea333acbf64e377be8e3ebbe1eb8e9ee3b17ae
airdropdaget <dagetairdrop@gmail.com>
2025-02-05 14:10:57 +0700

    2nd commit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Permasalahan V.2.0</title>
+  <title>Persuratan</title>
   <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css" />
   <link rel="stylesheet" href="node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.min.css" />
   <link rel="stylesheet" href="node_modules/flag-icon-css/css/flag-icon.min.css" />
